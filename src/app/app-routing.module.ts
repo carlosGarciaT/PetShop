93be5d0a18297b000998acdb8c6644c29f47cf37
Carlos <cgarciat@viewnext.com>
2020-11-25 16:41:49 +0100

    commit con las paginas y enrutado ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ErrorComponent } from './shared/error/error.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'detalles', loadChildren: () => import('./pages/detalles/detalles.module').then(m => m.DetallesModule) },
+  { path: 'edicion', loadChildren: () => import('./pages/edicion/edicion.module').then(m => m.EdicionModule) },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
